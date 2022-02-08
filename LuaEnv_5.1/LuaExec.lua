5cffd5fd84550bfacb89f66b6daf6ef86ac2e316
mydadcallsmemicky@hotmail.com <mydadcallsmemicky@hotmail.com>
2022-02-07 18:54:28 -0600

    bro wat

--- LuaEnv_5.1/LuaExec.lua
+++ LuaEnv_5.1/LuaExec.lua
@@ -1,13 +1,10 @@
-local function ShouldntWorkInLua(str)
-	print("func: "..str)
-	return "trolled"
-end
-print(ShouldntWorkInLua("joe mama"))
 local old
-old = hookfunction(ShouldntWorkInLua,(function(...)
-	return "no"
+old = hookfunction(deserialize,(function(...)
+	return print(...)
 end))
-print(ShouldntWorkInLua("joe mama"))
-old("no way it worked")
-ShouldntWorkInLua = old
-print(ShouldntWorkInLua("ah now it works"))
+
+local s = serialize([[
+	print("joemama")
+]])
+
+loadstring(s)
